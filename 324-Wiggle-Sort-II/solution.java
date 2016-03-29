96aa5d9d0fd01077753398260eae704bca321030
krunning <zhidongke@gmail.com>
2016-03-28 23:11:55 -0700

    [324. Wiggle Sort II][Runtime Error]committed by Krunning

--- 324-Wiggle-Sort-II/solution.java
+++ 324-Wiggle-Sort-II/solution.java
@@ -2,7 +2,7 @@ public class Solution {
     public void wiggleSort(int[] nums) {
         Arrays.sort(nums);
         int[] tmp = new int[nums.length];
-        int mid = (nums.length + 1) >> 1;
+        int mid = (nums.length) >> 1;
         int end = nums.length;
         for(int i = 0; i < nums.length; i++){
             tmp[i] = (i & 1) == 0 ? nums[--mid] : nums[--end];
