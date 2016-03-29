387a70212d82f26e49b04480edde6313796642f4
krunning <zhidongke@gmail.com>
2016-03-28 23:03:34 -0700

    [324. Wiggle Sort II][Wrong Answer]committed by Krunning

---
+++ 324-Wiggle-Sort-II/solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public void wiggleSort(int[] nums) {
+        Arrays.sort(nums);
+        int[] tmp = new int[nums.length];
+        int mid = nums.length >> 1;
+        int end = nums.length - 1;
+        for(int i = 0; i < nums.length; i++){
+            tmp[i] = (i & 1) == 0 ? nums[mid--] : nums[end--];
+        }
+        for(int i = 0; i < nums.length; i++){
+            nums[i] = tmp[i];
+        }
+    }
+}
