3fa31b6df8f85a9a76356c3bd35ffdb8af21943c
krunning <zhidongke@gmail.com>
2016-04-07 09:12:54 -0700

    [296. Best Meeting Point][Accepted]committed by Krunning

--- 296-Best-Meeting-Point/solution.java
+++ 296-Best-Meeting-Point/solution.java
@@ -1,35 +1,24 @@
 
 public class Solution {
         public int minTotalDistance(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-    
-        List<Integer> I = new ArrayList<>(m);
-        List<Integer> J = new ArrayList<>(n);
-    
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(grid[i][j] == 1){
-                    I.add(i);
-                    J.add(j);
+            int m = grid.length, n = grid[0].length;
+            int[] I = new int[m], J = new int[n];
+            for (int i=0; i<m; ++i)
+                for (int j=0; j<n; ++j)
+                    if (grid[i][j] == 1) {
+                        ++I[i];
+                        ++J[j];
+                    }
+            int total = 0;
+            for (int[] K : new int[][]{ I, J }) {
+                int i = 0, j = K.length - 1;
+                while (i < j) {
+                    int k = Math.min(K[i], K[j]);
+                    total += k * (j - i);
+                    if ((K[i] -= k) == 0) ++i;
+                    if ((K[j] -= k) == 0) --j;
                 }
             }
+            return total;
         }
-    
-        return getMin(I) + getMin(J);
-    }
-
-    private int getMin(List<Integer> list){
-        int ret = 0;
-    
-        Collections.sort(list);
-    
-        int i = 0;
-        int j = list.size() - 1;
-        while(i < j){
-            ret += list.get(j--) - list.get(i++);
-        }
-    
-        return ret;
-    }
 }
