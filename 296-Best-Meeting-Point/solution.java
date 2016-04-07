69de163ea9e685523554d1a8ee76ac3c1ad56716
krunning <zhidongke@gmail.com>
2016-04-07 09:13:14 -0700

    [296. Best Meeting Point][Accepted]committed by Krunning

--- 296-Best-Meeting-Point/solution.java
+++ 296-Best-Meeting-Point/solution.java
@@ -1,23 +1,23 @@
 
 public class Solution {
-        public int minTotalDistance(int[][] grid) {
+       public int minTotalDistance(int[][] grid) {
             int m = grid.length, n = grid[0].length;
-            int[] I = new int[m], J = new int[n];
-            for (int i=0; i<m; ++i)
-                for (int j=0; j<n; ++j)
-                    if (grid[i][j] == 1) {
-                        ++I[i];
-                        ++J[j];
-                    }
-            int total = 0;
-            for (int[] K : new int[][]{ I, J }) {
-                int i = 0, j = K.length - 1;
-                while (i < j) {
-                    int k = Math.min(K[i], K[j]);
-                    total += k * (j - i);
-                    if ((K[i] -= k) == 0) ++i;
-                    if ((K[j] -= k) == 0) --j;
+            int total = 0, Z[] = new int[m*n];
+            for (int dim=0; dim<2; ++dim) {
+                int i = 0, j = 0;
+                if (dim == 0) {
+                    for (int x=0; x<n; ++x)
+                        for (int y=0; y<m; ++y)
+                            if (grid[y][x] == 1)
+                                Z[j++] = x;
+                } else {
+                    for (int y=0; y<m; ++y)
+                        for (int g : grid[y])
+                            if (g == 1)
+                                Z[j++] = y;
                 }
+                while (i < --j)
+                    total += Z[j] - Z[i++];
             }
             return total;
         }
