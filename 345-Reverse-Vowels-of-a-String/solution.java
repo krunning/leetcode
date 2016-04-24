c9ea0a247312f59945e2359df84b562744cc0293
krunning <zhidongke@gmail.com>
2016-04-24 10:52:30 -0700

    [345. Reverse Vowels of a String][Accepted]committed by Krunning

--- 345-Reverse-Vowels-of-a-String/solution.java
+++ 345-Reverse-Vowels-of-a-String/solution.java
@@ -20,6 +20,6 @@ public class Solution {
     }
     private boolean isVowel(char c){
         return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
-            c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+            || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
     }
 }
